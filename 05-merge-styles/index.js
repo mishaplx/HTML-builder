63db4b0a29f3589e1a0ca06f853466fda674a1a7
mishaplx <olegorlov100oleg@gmail.com>
2021-11-04 19:28:26 +0300

    fix: move in project-dist

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,49 +1,56 @@
 const fs = require("fs");
 const path = require("path");
 
-
-fs.mkdir(path.join(__dirname, 'project-list'), err =>{
-    if (err) console.log(err);
-})
-
-let includes =  fs.readdirSync(path.join(__dirname, 'styles'));
-let res = []
+let includes = fs.readdirSync(path.join(__dirname, "styles"));
+let res = [];
 for (let i = 0; i < includes.length; i++) {
-    if (includes[i].slice(includes[i].indexOf('.') + 1, includes[i].length) == "css"){
-        res.push(includes[i])
-    }
-    
+  if (
+    includes[i].slice(includes[i].indexOf(".") + 1, includes[i].length) == "css"
+  ) {
+    res.push(includes[i]);
+  }
 }
-console.log(res);
-var startTime = Date.now();
-let  css = "";
-let filesLoaded = 0;
-let  fileTotal = res.length;
 
-    compileFile = function() {
-        fs.readFile(path.join(__dirname, "styles", res[filesLoaded]),  'utf8', function readFileResponse(err, data) {
-            if(err) {
-                console.log(err);
-            } else {
-                css += data;
-                filesLoaded++;
-                if(filesLoaded >= fileTotal) {
-                    writeIndex();
-                    return;
-                }
-                compileFile();
-            }
-        });
-    },
-    writeIndex = function() {
-            //console.log(html);
-            fs.writeFile(path.join(__dirname, 'bundle.css'), css, function(err) {
-                if(err) {
-                    console.log("file not written: " + err);
-                } else {
-                    console.log("Compiled file in " + String(Date.now() - startTime) + "ms");
-                }
-            });
+var startTime = Date.now();
+let css = "";
+let filesLoaded = 0;
+let fileTotal = res.length;
+
+(compileFile = function () {
+  fs.readFile(
+    path.join(__dirname, "styles", res[filesLoaded]),
+    "utf8",
+    function readFileResponse(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        css += data;
+        filesLoaded++;
+        if (filesLoaded >= fileTotal) {
+          writeIndex();
+          var oldPath = path.join(__dirname, "bundle.css");
+          var newPath = path.join(__dirname, "project-dist", "bundle.css");
+
+          fs.rename(oldPath, newPath, function (err) {
+            if (err) throw err;
+          });
+          return;
+        }
+        compileFile();
+      }
     }
+  );
+}),
+  (writeIndex = function () {
+    fs.writeFile(path.join(__dirname, "bundle.css"), css, function (err) {
+      if (err) {
+        console.log("file not written: " + err);
+      } else {
+        console.log(
+          "Compiled file in " + String(Date.now() - startTime) + "ms"
+        );
+      }
+    });
+  });
 
 compileFile();
