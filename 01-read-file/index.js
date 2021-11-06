65cf0e67df79af959d8fefe00edbc5b8a048b87f
mishaplx <olegorlov100oleg@gmail.com>
2021-11-06 22:06:16 +0300

    fix:02

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-// let readStream = fs.createReadStream(__dirname + "/text.txt")
-// readStream.pipe(process.stdout)
 var stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
- 
+
 stream.on('readable', function(){
     var data = stream.read();
     console.log(data);
 });
+// let readStream = fs.createReadStream(__dirname + "/text.txt")
+// readStream.pipe(process.stdout)
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,15 +3,26 @@ const fs = require("fs");
 
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
+const path = require("path/posix");
 
+function fileHandler(){
+
+  fs.open(path.join(__dirname, 'out.txt'), 'w', (err) => {
+      if(err) throw err;
+      
+  });
+  
+}
+
+console.log("Hello, please enter text");
+fileHandler()
 const rl = readline.createInterface({ input, output });
 let _input = ''
-console.log("Hello, please enter text");
 rl.on("line", (input) => {
-    if (input == 'exit'){
-      console.log('good buy');
-        process.exit()
-    }
+  if (input == 'exit'){
+    console.log('good buy');
+    process.exit()
+  }
   _input += input + '\n';
   let writeSteam = fs.createWriteStream(__dirname + "/out.txt");
   writeSteam.write(_input);
