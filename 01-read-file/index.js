dfc9ece61e8fc68ea87772cb1f972f285d9ed6f8
mishaplx <olegorlov100oleg@gmail.com>
2021-11-06 22:00:00 +0300

    fix: 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,10 @@
 const fs = require('fs')
-let readStream = fs.createReadStream(__dirname + "/text.txt")
-readStream.pipe(process.stdout)
+const path = require('path')
+// let readStream = fs.createReadStream(__dirname + "/text.txt")
+// readStream.pipe(process.stdout)
+var stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+ 
+stream.on('readable', function(){
+    var data = stream.read();
+    console.log(data);
+});
